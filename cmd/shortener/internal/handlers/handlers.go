94fd990e551ab6a20cdde1ffdd1607343e545126
Ermoshin Maksim <maksim_ermoshin@mail.ru>
2026-03-29 11:31:26 +0300

    Use chi router

--- cmd/shortener/internal/handlers/handlers.go
+++ cmd/shortener/internal/handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/Starkiller2000Space-Projects/shortener/cmd/shortener/internal/storage"
+	"github.com/go-chi/chi/v5"
 )
 
 type Handler struct {
@@ -22,7 +23,7 @@ func (h *Handler) IdHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	id := r.PathValue("id")
+	id := chi.URLParam(r, "id")
 	w.Header().Set("Content-Type", "text/plain")
 	url, ok := h.store.Get(id)
 	if !ok {
